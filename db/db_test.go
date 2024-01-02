40406e37b273115a8cdcee9ddf4b6fead72227a6
simpleshapeapps/dev <sebastiangarus@protonmail.com>
2024-01-02 22:21:02 +0100

    create table db test

--- db/db_test.go
+++ db/db_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"json2sql/generators"
+	"json2sql/types"
 	"os"
 	"testing"
 
@@ -14,6 +16,8 @@ var port = getEnvOrDefault("POSTGRES_PORT", "5432")
 var user = getEnvOrDefault("POSTGRES_USER", "postgres")
 var password = getEnvOrDefault("POSTGRES_PASSWORD", "postgres")
 var databaseName = getEnvOrDefault("POSTGRES_DATABASE", "postgres")
+
+var db *sqlx.DB
 
 func TestMain(m *testing.M) {
 	if host != "" {
@@ -57,10 +61,158 @@ func TestDbConnection(t *testing.T) {
 	}
 }
 
+func TestCreateTable(t *testing.T) {
+	types.Register(parentThing)
+	types.Register(childThing)
+	types.Register(otherThing)
+	defer types.Clear()
+
+	generator := generators.CreateTable{
+		Thing: parentThing,
+	}
+
+	sqls, err := generator.GetSql()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	doAndRollback(func(tx *sqlx.Tx) {
+		for _, sql := range sqls {
+			tx.MustExec(sql)
+		}
+
+		things := []types.ThingConfig{parentThing, childThing, otherThing}
+		for _, thing := range things {
+			tableName := thing.GetTableName()
+			rows, err := tx.Queryx("SELECT * FROM " + tableName)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if rows.Next() {
+				t.Fatalf("No rows in %s expected", tableName)
+			}
+		}
+	})
+}
+
+func getDb() *sqlx.DB {
+	if db == nil {
+		connectionString := fmt.Sprintf("host=%s port=%s user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+			host, port, user, password, databaseName)
+
+		db = sqlx.MustConnect("postgres", connectionString)
+	}
+
+	return db
+}
+
+func doAndRollback(callback func(tx *sqlx.Tx)) {
+	db := getDb()
+	tx := db.MustBegin()
+	callback(tx)
+	tx.Rollback()
+}
+
 func getEnvOrDefault(envName string, def string) string {
 	envValue := os.Getenv(envName)
 	if envValue == "" {
 		return def
 	}
 	return envValue
+}
+
+var parentThing = types.ThingConfig{
+	Name: "parentThing",
+	Fields: map[string]types.FieldConfig{
+		"primary_key": {
+			Name: "primary_key",
+			Type: types.PRIMARY_KEY,
+		},
+		"string": {
+			Name: "string",
+			Type: types.STRING,
+		},
+		"number": {
+			Name: "number",
+			Type: types.NUMBER,
+		},
+		"boolean": {
+			Name: "boolean",
+			Type: types.BOOLEAN,
+		},
+		"date": {
+			Name: "date",
+			Type: types.DATE,
+		},
+		"thing": {
+			Name:          "thing",
+			Type:          types.THING,
+			TypeThingName: "otherThing",
+		},
+		"oneToMany": {
+			Name: "oneToMany",
+			Type: types.RELATION,
+			Relation: types.ThingRelation{
+				Type:           types.ONE_TO_MANY,
+				OtherThingName: "childThing",
+				OtherFieldName: "manyToOne",
+			},
+		},
+	},
+}
+
+var childThing = types.ThingConfig{
+	Name: "childThing",
+	Fields: map[string]types.FieldConfig{
+		"primary_key": {
+			Name: "primary_key",
+			Type: types.PRIMARY_KEY,
+		},
+		"string": {
+			Name: "string",
+			Type: types.STRING,
+		},
+		"number": {
+			Name: "number",
+			Type: types.NUMBER,
+		},
+		"boolean": {
+			Name: "boolean",
+			Type: types.BOOLEAN,
+		},
+		"date": {
+			Name: "date",
+			Type: types.DATE,
+		},
+		"thing": {
+			Name:          "thing",
+			Type:          types.THING,
+			TypeThingName: "otherThing",
+		},
+		"manyToOne": {
+			Name: "manyToOne",
+			Type: types.RELATION,
+			Relation: types.ThingRelation{
+				Type:           types.MANY_TO_ONE,
+				OtherThingName: "parentThing",
+				OtherFieldName: "oneToMany",
+			},
+		},
+	},
+}
+
+var otherThing = types.ThingConfig{
+	Name: "otherThing",
+	Fields: map[string]types.FieldConfig{
+		"primary_key": {
+			Name: "primary_key",
+			Type: types.PRIMARY_KEY,
+		},
+		"string": {
+			Name: "string",
+			Type: types.STRING,
+		},
+	},
 }
